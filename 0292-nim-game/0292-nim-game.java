be9c39c9a27d26eb188fdfbfca106363e89e9e14
Bevin Iseac.A <166494918+BevinIseac@users.noreply.github.com>
2024-08-06 13:48:50 +0530

    Time: 0 ms (100%), Space: 39.9 MB (87.96%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -3,11 +3,8 @@ class Solution {
         if(n%4==0){
             return false;
         }
-        if((n%3==0||n%3==1||n%3==2)&&(n%4!=0)){
-            return true;
-        }
         else{
-            return false;
+            return true;
         }
     }
 }
