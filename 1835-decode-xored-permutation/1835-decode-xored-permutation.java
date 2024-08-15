8bfb810258289f90a15f3f419edc64963e64c4b1
Bevin Iseac.A <166494918+BevinIseac@users.noreply.github.com>
2024-08-15 23:34:25 +0530

    Time: 2 ms (100%), Space: 63.3 MB (86.33%) - LeetHub

--- 1835-decode-xored-permutation/1835-decode-xored-permutation.java
+++ 1835-decode-xored-permutation/1835-decode-xored-permutation.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] decode(int[] encoded) {
         int n=encoded.length+1;
-        int arr[]=new int[n];
-        int a=0;
-        int b=0;
+        int ans[]=new int[n];
+        int all=0;
+        int encod=0;
         for(int i=1;i<=n;i++)
-            a=a^(i);
+            all=all^(i);
         for(int i=1;i<n;i+=2)
-            b=b^encoded[i];
-        int first=a^b;
-        arr[0]=first;
+            encod=encod^encoded[i];
+        int first=all^encod;
+        ans[0]=first;
         for(int i=1;i<n;i++)
         {
-            arr[i]=arr[i-1]^encoded[i-1];
+            ans[i]=ans[i-1]^encoded[i-1];
         }
-        return arr;
+        return ans;
     }
 }
