07120c52991b68e84cbf1f420ad455126beac25b
Bevin Iseac.A <166494918+BevinIseac@users.noreply.github.com>
2024-09-05 15:43:07 +0530

    Time: 9 ms (99.94%), Space: 55.9 MB (87.03%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,42 +1,27 @@
-
-
 class Solution {
-    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        List<List<Pair>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            double prob = succProb[i];
-            graph.get(u).add(new Pair(v, prob));
-            graph.get(v).add(new Pair(u, prob));
-        }
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
         double[] maxProb = new double[n];
-        maxProb[start] = 1.0;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (Pair neighbor : graph.get(node)) {
-                int nextNode = neighbor.node;
-                double edgeProb = neighbor.probability;
-                double newProb = maxProb[node] * edgeProb;
-                if (newProb > maxProb[nextNode]) {
-                    maxProb[nextNode] = newProb;
-                    queue.add(nextNode);
+        maxProb[start_node] = 1.0;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean updated = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
+
+                if (maxProb[u] * prob > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * prob;
+                    updated = true;
+                }
+                if (maxProb[v] * prob > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * prob;
+                    updated = true;
                 }
             }
+            if (!updated) break;
         }
-        return maxProb[end];
-    }
-    class Pair {
-        int node;
-        double probability;
-        Pair(int node, double probability) {
-            this.node = node;
-            this.probability = probability;
-        }
+
+        return maxProb[end_node];
     }
 }
