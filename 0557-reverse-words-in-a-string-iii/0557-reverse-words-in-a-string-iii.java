ae5834a9cfe26a71a67980026b19036127cb6001
Bevin Iseac.A <166494918+BevinIseac@users.noreply.github.com>
2024-09-05 15:40:37 +0530

    Time: 47 ms (5.69%), Space: 46.1 MB (5.01%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,5 +1,15 @@
 class Solution {
     public String reverseWords(String s) {
-        
+        String[] str = s.split("\\s+");
+        String res = "";
+        for(int i=0;i<str.length;i++){
+            String a = str[i];
+            String b ="";
+            for(int j=a.length()-1;j>=0;j--){
+                b = b+a.charAt(j);
+            }
+            res = res + b + " ";
+        }
+        return res.trim();
     }
 }
